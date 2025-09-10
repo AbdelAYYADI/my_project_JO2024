5ee6e76e6c241a37b77bf9675c56362c91314cee
AYYADI-Abderrahmane <ab.ayyadi@gmail.com>
2025-09-10 13:17:29 +0200

    Màj Accueil and Events

--- migrations/Version20250907125622.php
+++ migrations/Version20250907125622.php
@@ -19,15 +19,19 @@ final class Version20250907125622 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        /*
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, sport_id INT NOT NULL, name VARCHAR(30) NOT NULL, title VARCHAR(100) NOT NULL, date_event DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', program_event VARCHAR(500) NOT NULL, location VARCHAR(120) NOT NULL, INDEX IDX_3BAE0AA7AC78BCF8 (sport_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7AC78BCF8 FOREIGN KEY (sport_id) REFERENCES sport (id)');
+        */
     }
 
     public function down(Schema $schema): void
     {
+        /*
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7AC78BCF8');
         $this->addSql('DROP TABLE event');
+        */
     }
 }
